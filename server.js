8ea8ae25a181646c2ec6390a13f942226cf22e60
denisha32 <43677295+denisha32@users.noreply.github.com>
2021-11-25 00:05:00 -0500

    fix url

--- server.js
+++ server.js
@@ -5,10 +5,10 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static('./dist/employeeManagerApp'));
+app.use(express.static('./dist/employee-manager-app'));
 
 app.get('/*', (req, res) =>
-    res.sendFile('index.html', {root: 'dist/employeeManagerApp/'}),
+    res.sendFile('index.html', {root: 'dist/employee-manager-app/'}),
 );
 
 // Start the app by listening on the default Heroku port
