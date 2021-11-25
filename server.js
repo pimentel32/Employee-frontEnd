e7471f208a94c197fafd8d8a0cc94f65cb098275
denisha32 <43677295+denisha32@users.noreply.github.com>
2021-11-24 19:52:27 -0500

    fix express

--- server.js
+++ server.js
@@ -4,12 +4,12 @@ const path = require('path');
 
 const app = express();
 
-// Serve only the static files from the dist directory
+// Serve only the static files form the dist directory
 app.use(express.static('./dist/employeeManagerApp'));
 
-app.use('/*', (req, res) =>
-    res.sendFile('index.html', {root: 'dist/employeeManagerApp/'})
+app.get('/*', (req, res) =>
+    res.sendFile('index.html', {root: 'dist/employeeManagerApp/'}),
 );
 
-//Start the app by listening on the default Heroku port
+// Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
