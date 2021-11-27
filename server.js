1b877acb8c486c3c63e8cfa0ade4944bae2829bd
denisha32 <43677295+denisha32@users.noreply.github.com>
2021-11-26 20:43:58 -0500

    server/package readjustment

--- server.js
+++ server.js
@@ -5,11 +5,12 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static('./dist/employee-manager-app'));
+app.use(express.static(__dirname + '/dist/employee-manager-app'));
 
-app.get('/*', (req, res) =>
-    res.sendFile('index.html', {root: 'dist/employee-manager-app/'}),
-);
+app.get('/*', function(req,res) {
+    res.sendFile(path.join(__dirname+
+    '/dist/employee-manager-app/index.html'));
+});
 
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
