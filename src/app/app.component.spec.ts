063ea10257f500d22b65ac13a5b97d8a9361c9ff
denisha32 <43677295+denisha32@users.noreply.github.com>
2021-11-26 21:19:27 -0500

    modified:   .angular/cache/angular-webpack/b8e25a76fe3c4ebb763e4b80cb10e2f870b485b2/0.pack 	modified:   .angular/cache/angular-webpack/b8e25a76fe3c4ebb763e4b80cb10e2f870b485b2/1.pack 	modified:   .angular/cache/angular-webpack/b8e25a76fe3c4ebb763e4b80cb10e2f870b485b2/index.pack 	modified:   .angular/cache/angular-webpack/b8e25a76fe3c4ebb763e4b80cb10e2f870b485b2/index.pack.old 	modified:   README.md 	modified:   angular.json 	deleted:    dist/employeeManagerApp/3rdpartylicenses.txt 	deleted:    dist/employeeManagerApp/favicon.ico 	deleted:    dist/employeeManagerApp/index.html 	deleted:    dist/employeeManagerApp/main.29a39c5440b9fc7d035c.js 	deleted:    dist/employeeManagerApp/polyfills.00e5a3b55d9606759664.js 	deleted:    dist/employeeManagerApp/runtime.f762a6a6fd10012d3845.js 	deleted:    dist/employeeManagerApp/styles.2844293afab00f5b2cb3.css 	modified:   docs/index.html 	deleted:    docs/main.677a0b0c80d8bc7c.js 	deleted:    docs/polyfills.ae3b224b9f1ddb89.js 	deleted:    docs/runtime.53183b3c324e2972.js 	modified:   index.html 	modified:   karma.conf.js 	modified:   src/app/app.component.spec.ts

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -16,16 +16,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'employeeManagerApp'`, () => {
+  it(`should have as title 'employee-manager-app'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('employeeManagerApp');
+    expect(app.title).toEqual('employee-manager-app');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('employeeManagerApp app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('employee-manager-app app is running!');
   });
 });
